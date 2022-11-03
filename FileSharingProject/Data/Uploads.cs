69ded7ef54fe2b82dcb81e4c411df8872d8d8eaa
murad <msn0033@gmail.com>
2022-11-03 12:27:23 +0300

    Add project files.

---
+++ FileSharingProject/Data/Uploads.cs
@@ -0,0 +1,15 @@
+﻿namespace FileSharingProject.Data
+{
+    public class Uploads
+    {
+        public Uploads()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        public string Id { get; set; }
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public decimal Size { get; set; }
+        public string UserId { get; set; }
+    }
+}
